461a91631c680b0eb92170508b17fadf47366cda
kim123 <floreskimbie@gmail.com>
2014-06-06 17:16:21 +0800

    CREATED NumberUtilities.java

---
+++ src/com/is/utilities/NumberUtility.java
@@ -0,0 +1,21 @@
+package com.is.utilities;
+
+public class NumberUtilities{
+
+  public static boolean isValidNumberDecimal(String given){
+    boolean isValidNumber = true;
+    if (given==null) {
+      return false;
+    }
+    if (given.length()==0) {
+      return false;
+    }
+    try {
+      Double.parseDouble(given);
+    } catch (NumberFormatException nfe) {
+      isValidNumber = false;
+    }
+    return isValidNumber;
+  }
+  
+}
