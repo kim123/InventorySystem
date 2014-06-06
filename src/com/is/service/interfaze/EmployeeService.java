b566150f88e21898e73ce4899c0ae833be59e756
kim123 <floreskimbie@gmail.com>
2014-06-06 17:55:03 +0800

    CREATED EmployeeService.java

---
+++ src/com/is/service/interfaze/EmployeeService.java
@@ -0,0 +1,9 @@
+package com.is.service.interfaze;
+
+public interface EmployeeService{
+  
+  String checkIn(String userId);
+  String checkOut(String userId;
+  String recordJournal(String userId, StringBuilder journalEntry);
+  
+}
