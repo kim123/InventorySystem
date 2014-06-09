fa113dcad80d8d537b229a737e36e8752e5ae96f
kim123 <floreskimbie@gmail.com>
2014-06-09 12:55:26 +0800

    CREATED StocksOnHand.java

---
+++ src/com/is/service/interfaze/StocksOnHand.java
@@ -0,0 +1,8 @@
+package com.is.service.interfaze;
+
+public interface StocksOnHand{
+
+    Page viewStocksOnHandLogs(Map<String, Object) constraints);
+    String updateStocksOnHand(StocksOnHand stocks);
+
+}
