a31fcf06472ec24558791fbd16b8000e020a5eb9
kim123 <floreskimbie@gmail.com>
2014-06-09 11:48:12 +0800

    UPDATED Inventory 1.1

--- src/com/is/service/interfaze/InventoryService.java
+++ src/com/is/service/interfaze/InventoryService.java
@@ -3,6 +3,7 @@ package com.is.service.interfaze;
 public interface InventoryService{
   
   Page viewCurrentInventory(Map<String, Object> constraints);
+  List<Inventory> getInventoryStatusList(Map<String, Object> constraints);
   String addStocks(Inventory inventory);
   
 }
