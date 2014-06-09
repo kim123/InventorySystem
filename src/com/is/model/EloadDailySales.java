cf49a429cd884fc7c45a804836bbb95049771b26
kim123 <floreskimbie@gmail.com>
2014-06-09 12:58:48 +0800

    CREATED EloadDailySales.java

---
+++ src/com/is/model/EloadDailySales.java
@@ -0,0 +1,17 @@
+package com.is.model;
+
+import java.sql.Timestamp;
+import java.math.BigDecimal;
+
+public class EloadDailySales{
+
+    private Integer eloadDailySalesId;
+    private Integer productId;
+    private Integer quantity;
+    private BigDecimal total;
+    private Timestamp updatedDate;
+    private String updatedBy;
+    
+    //getters and setters
+
+}
