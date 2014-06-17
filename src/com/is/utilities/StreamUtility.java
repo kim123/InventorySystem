571a53e6a0206391ff25aa851175810d96f8ad39
kim123 <floreskimbie@gmail.com>
2014-06-17 23:13:55 +0800

    EDITED StreamUtility.java

--- src/com/is/utilities/StreamUtility.java
+++ src/com/is/utilities/StreamUtility.java
@@ -1,23 +1,31 @@
 package com.is.utilities;
 
-public class StreamUtility{
+import java.io.IOException;
+import java.io.InputStream;
 
-    public static void writeToBrowser(InputStream inputStream, HttpServletResponse response, String mimeType, String filename){
-      
-        byte[] buffer = new byte[inputStream.available()];
-        int bytesRead = 0;
-        
-        response.reset();
-        response.setHeader("Content-disposition","inline;filename="+filename);
-        response.setContentType(mimeType);
-        
-        while ((bytes = inputStream.read(buffer)) != -1) {
-            response.getOutputStream().write(buffer, 0, bytesRead);
-        }
-        
-        response.getOutputStream().flush();
-        response.getOutputStream().close();
-      
-    }
+import javax.servlet.http.HttpServletResponse;
+
+public class StreamUtility {
+
+	public static void writeToBrowser(InputStream inputStream,HttpServletResponse response, String mimeType, String filename) {
+		try {
+			byte[] buffer = new byte[inputStream.available()];
+			int bytesRead = 0;
+
+			response.reset();
+			response.setHeader("Content-disposition", "inline;filename="+ filename);
+			response.setContentType(mimeType);
+
+			while ((bytesRead = inputStream.read(buffer)) != -1) {
+				response.getOutputStream().write(buffer, 0, bytesRead);
+			}
+			response.getOutputStream().flush();
+			response.getOutputStream().close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
 
 }
